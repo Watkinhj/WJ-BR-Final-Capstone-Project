1daa422eaa5e1265644c5aaadc6ff01b8f696ef5
Watkinhj <117215234+Watkinhj@users.noreply.github.com>
2024-05-13 17:38:27 -0400

    Sound tweaking

--- Capstone Project/Assets/Scripts/Player Scripts/CurrencyPickup.cs
+++ Capstone Project/Assets/Scripts/Player Scripts/CurrencyPickup.cs
@@ -7,13 +7,29 @@ public class CurrencyPickup : MonoBehaviour
     public enum PickupObject {COIN};
     public PickupObject currentObject;
     public int pickupQuantity;
+    private AudioSource pickupSound;
+
+    private void Start()
+    {
+        //Grabs the audio source to make shit work
+        pickupSound = GetComponent<AudioSource>();
+        if (!pickupSound)
+        {
+            Debug.LogError("missing audio source on pickup");
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.name == "Player")
         {
             PlayerStats.playerStats.AddCurrency(this);
-            Destroy(gameObject);
+
+            //PLAY SOUND
+            pickupSound.Play();
+
+            //DESTROY SOUND AFTER DELAY
+            Destroy(gameObject, pickupSound.clip.length);
         }
     }
 }
